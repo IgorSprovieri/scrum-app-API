6c0efb34787802d40b53e4058bf6dc6d264cf48e
Igor Sprovieri <igorsprovieri@MacBook-Air-de-Igor.local>
2023-03-07 09:42:11 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import db from "./db";
+import routes from "./routes";
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, async () => {
+  try {
+    await db.sequelize.authenticate();
+    console.log("App running and db connected");
+  } catch (error) {
+    console.log(error);
+  }
+});
